f90a06641d6412ba1d374c17795e64fed12363e2
RajvinderYogi <rajvinderyogi@yahoo.com>
2017-04-12 01:46:53 -0400

    Added Text to banner

--- Scripts/core/create-ad.js
+++ Scripts/core/create-ad.js
@@ -1,11 +1,43 @@
 (function () {
+    var ProjectsAd = document.getElementById("ProjectsAd");
+    var stage;
+    var AdText;
+    /**
+     *
+     * @method Start
+     * @returns void
+     */
     function Start() {
+        stage = new createjs.Stage(ProjectsAd);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
         Main();
     }
-    function Update() {
+    /**
+     *
+     * @method Update
+     * @returns void
+     */
+    function Update(event) {
+        // AdText.rotation += 1;
+        AdText.x += 0.1;
+        AdText.y -= 0.1;
+        stage.update();
     }
+    /**
+     *
+     * @method Main
+     * @returns void
+     */
     function Main() {
         console.log("This is my Portfolio!");
+        AdText = new createjs.Text("See my Project Work!", "50px Times", "lime");
+        AdText.textBaseline = "hanging";
+        AdText.regX = AdText.getMeasuredWidth() * 0.5;
+        AdText.regY = AdText.getMeasuredHeight() * 0.5;
+        AdText.y = 35;
+        AdText.x = 230;
+        stage.addChild(AdText);
     }
     window.onload = Start;
 })();
--- Scripts/core/create-ad.ts
+++ Scripts/core/create-ad.ts
@@ -1,15 +1,45 @@
 (function(){
-
+let ProjectsAd:HTMLElement = document.getElementById("ProjectsAd")
+let stage:createjs.Stage;
+let AdText:createjs.Text;
+/**
+ * 
+ * @method Start
+ * @returns void
+ */
 function Start(){
+    stage = new createjs.Stage(ProjectsAd);
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.on("tick", Update)
     Main();
 }
-
-function Update(){
-
+/**
+ * 
+ * @method Update
+ * @returns void
+ */
+function Update(event:createjs.Event):void {
+    // AdText.rotation += 1;
+    AdText.x += 0.1;
+    AdText.y -= 0.1;
+    stage.update();
 }
+/**
+ * 
+ * @method Main
+ * @returns void
+ */
 
-function Main(){
-    console.log("This is my Portfolio!")
+function Main():void{
+    console.log("This is my Portfolio!");
+
+    AdText = new createjs.Text("See my Project Work!","50px Times", "lime");
+    AdText.textBaseline = "hanging";
+    AdText.regX = AdText.getMeasuredWidth()*0.5;
+    AdText.regY = AdText.getMeasuredHeight()*0.5;
+    AdText.y=35;
+    AdText.x=230;
+    stage.addChild(AdText);
 
 }
 window.onload = Start;
